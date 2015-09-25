287911b124be29146c25f889908a36911f0d30b7
Jason Bethel <jlbethel@gmail.com>
2015-09-25 09:07:49 -0700

    add route for home.html to app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 var signIn = angular.module('signIn', ['ui.router']);
+
+signIn.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    url:"",
+    templateUrl: "partials/home.html",
+  });
+});
