7923279ae763823f48a23d01fe3774bad86e2893
Jason Bethel <jlbethel@gmail.com>
2015-09-25 09:29:42 -0700

    create route to students.html in app.js

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var signIn = angular.module('signIn', ['ui.router']);
 signIn.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider.state('home', {
     url:"",
-    templateUrl: "partials/home.html",
+    templateUrl: "partials/home.html"
+  });
+
+  $stateProvider.state('students.html', {
+    url: "/students",
+    templateUrl: "partials/students.html",
+    controller: "SutdentsCtrl"
   });
 });
